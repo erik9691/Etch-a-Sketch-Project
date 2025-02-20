636f392f4a4f80e839facfd385c739a77cf857db
erik9691 <gagwaffle@gmail.com>
2025-02-20 00:53:23 -0300

    Can now paint on grid

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const container = document.querySelector(".container");
 
 createBoxes();
 
+const boxes = document.querySelectorAll(".box");
+
 function createBoxes()
 {
     for (let i = 0; i < (16*16); i++)
@@ -11,3 +13,11 @@ function createBoxes()
         container.appendChild(box);
     }
 }
+
+boxes.forEach((box) => 
+{
+    box.addEventListener("mouseenter", function(e)
+    {
+        e.target.classList.add("painted");
+    });
+});
